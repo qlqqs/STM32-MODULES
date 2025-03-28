381b7898ac90a7ab6a988b0745bc2e1b2aedab3c
qlqqs <qcts@qlqqs.com>
2025-03-28 11:34:49 +0800

    Update bsp_uart.h

--- UART/bsp_uart.h
+++ UART/bsp_uart.h
@@ -5,26 +5,26 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-/************************************** ����Ϊ�����궨�� ******************************************/
-/******************* ��ֲ��Ҫ�޸ĵĺ궨�� ******************/
-/* �����豸ID���� */
+/************************************** 以下为参数宏定义 ******************************************/
+/******************* 移植需要修改的宏定义 ******************/
+/* 串口设备ID定义 */
 #define DEV_UART1                1
 #define DEV_UART3                3
 #define DEV_UART4                4
 #define DEV_UART5                5
 
-/* �����豸�������� */
-#define UART_DEVICE_MAX_NUM      5                                 /* ��󴮿��豸���� �������ΪUART5*/
-#define UART_DEVICE_ARRAY_NUM    (UART_DEVICE_MAX_NUM + 1)         /* �����豸�����С UART_DEVICE_MAX_NUM + 1*/
-#define UART_COUNT_ARRAY_SIZE    ((UART_DEVICE_MAX_NUM + 1) * 2)   /* �����շ����������С 2*UART_DEVICE_MAX_NUM */
-/******************* ��ֲ��Ҫ�޸ĵĺ궨�� ******************/
+/* 串口设备数量定义 */
+#define UART_DEVICE_MAX_NUM      5                                 /* 最大串口设备数量 这里最大为UART5*/
+#define UART_DEVICE_ARRAY_NUM    (UART_DEVICE_MAX_NUM + 1)         /* 串口设备数组大小 UART_DEVICE_MAX_NUM + 1*/
+#define UART_COUNT_ARRAY_SIZE    ((UART_DEVICE_MAX_NUM + 1) * 2)   /* 串口收发计数数组大小 2*UART_DEVICE_MAX_NUM */
+/******************* 移植需要修改的宏定义 ******************/
 
-/* �������ò�����״̬���� */
-#define UART_SET_BAUDRATE_OK     0                                 /* ���ò����ʳɹ� */
-#define UART_SET_BAUDRATE_ERROR  1                                 /* ���ò�����ʧ�� */
+/* 串口设置波特率状态定义 */
+#define UART_SET_BAUDRATE_OK     0                                 /* 设置波特率成功 */
+#define UART_SET_BAUDRATE_ERROR  1                                 /* 设置波特率失败 */
 
-/************************************** ����Ϊ�������� ******************************************/
-/* �ⲿ�������� */
+/************************************** 以下为函数声明 ******************************************/
+/* 外部函数声明 */
 void bsp_uart_dmatx_config(uint8_t uart_id, uint8_t *mem_addr, uint32_t mem_size);
 void bsp_uart_dmarx_config(uint8_t uart_id, uint8_t *mem_addr, uint32_t mem_size);
 void uart_dma_init(uint8_t uart_id, uint8_t *mem_addr, uint32_t mem_size);
